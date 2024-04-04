3635fa4ee9080fc78122e46c64b61a077c6137c6
121038217 <125855991+121038217@users.noreply.github.com>
2024-04-04 12:21:41 -0600

    Creación de la segunda interfaz y conexión con el botón de crear cuenta

--- app/src/main/java/com/example/myapplication/registro.kt
+++ app/src/main/java/com/example/myapplication/registro.kt
@@ -8,16 +8,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
-class MainActivity : ComponentActivity() {
+class registro : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
+        setContentView(R.layout.registro)
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
@@ -30,24 +27,8 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-    fun abrirregistro(view: View){
-        val intent= Intent(this,registro::class.java).apply{}
+    fun regresarlogin(view: View){
+        val intent= Intent(this,MainActivity::class.java).apply{}
         startActivity(intent)
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
